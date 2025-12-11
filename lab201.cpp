b9d57f29b359cba9a0368228a1a372c2183fd54f
Danuphat Komek <freetosoon@gmail.com>
2025-12-11 20:42:50 +0700

    update

--- lab201.cpp
+++ lab201.cpp
@@ -4,17 +4,14 @@ void reverse(char str1[], char str2[])
 {
     int i, len = 0 ;
 
-    // หาความยาว string
     while (str1[len] != '\0') {
         len++ ;
     }
 
-    // คัดลอกจากท้ายไปหน้า
     for (i = 0; i < len; i++) {
         str2[i] = str1[len - 1 - i] ;
     }
 
-    // ปิดท้ายด้วย null
     str2[len] = '\0' ;
 }
 
@@ -25,7 +22,6 @@ int main()
 
     fgets(text, 50, stdin) ;
 
-    // ตัด newline ออก (ถ้ามี)
     int i = 0;
     while (text[i] != '\0') {
         if (text[i] == '\n') {
